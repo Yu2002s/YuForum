28d5dcabefc6aaa4f59f97a89931bf54b0e94574
jdy2002 <jiangdongyu54@gmail.com>
2022-11-25 10:01:36 +0800

    update

--- src/test/java/com/drny/forum/Test.java
+++ src/test/java/com/drny/forum/Test.java
@@ -1,2 +1,21 @@
-package com.drny.forum;public class Test {
+package com.drny.forum;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int[] array = {12, 2, 100, 212, 120};
+        int max = max(array);
+        System.out.println("max = " + max);
+    }
+
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
 }
